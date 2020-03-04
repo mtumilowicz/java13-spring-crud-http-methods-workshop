59904804a548ab0f064db8eeff4a5624e726ccc9
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-04 20:51:08 +0100

    options

--- src/main/java/app/ProcessConfigController.java
+++ src/main/java/app/ProcessConfigController.java
@@ -1,7 +1,8 @@
 package app;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -26,5 +27,13 @@ public class ProcessConfigController {
     @PatchMapping("/{id}")
     public String xxx(@RequestBody Map<String, String> props, @PathVariable String id) {
         return String.valueOf(processConfigService.update(props, id));
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    ResponseEntity<?> collectionOptions() {
+        return ResponseEntity
+                .ok()
+                .allow(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS, HttpMethod.PATCH)
+                .build();
     }
 }
