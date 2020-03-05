42c36d76e0942ce8778f4236093367342c6eed77
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-05 15:46:04 +0100

    testing put

--- src/test/groovy/app/functional/FunctionalTests.groovy
+++ src/test/groovy/app/functional/FunctionalTests.groovy
@@ -1,5 +1,7 @@
 package app.functional
 
+import app.gateway.output.ProcessConfigApiOutput
+import org.apache.coyote.Response
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -33,5 +35,30 @@ class FunctionalTests extends Specification {
                         ]))
         )
         .andExpect(status().isOk())
+    }
+
+    def 'put'() {
+        given:
+       def xxx = mockMvc.perform(
+                post('/app')
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(RequestMapper.asJsonString([
+                                props: [a: 'a']
+                        ]))
+        )
+                .andExpect(status().isOk())
+        .andReturn()
+
+        ProcessConfigApiOutput yyy = ResponseMapper.parseResponse(xxx, ProcessConfigApiOutput)
+
+        expect:
+        mockMvc.perform(
+                put("/app/$yyy.id")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(RequestMapper.asJsonString([
+                                props: [a: 'b']
+                        ]))
+        )
+                .andExpect(status().isOk())
     }
 }
