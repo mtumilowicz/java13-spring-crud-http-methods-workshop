5340e04b7de71bf06977957a68b0494e23d6bcb5
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-13 00:45:32 +0100

    merging configurations for workshops

--- src/main/java/app/infrastructure/configuration/ProcessConfigConfiguration.java
+++ src/main/java/app/infrastructure/configuration/ProcessConfigConfiguration.java
@@ -2,6 +2,7 @@ package app.infrastructure.configuration;
 
 import app.domain.ProcessConfigRepository;
 import app.domain.ProcessConfigService;
+import app.domain.ProcessConfigServiceWorkshop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +12,10 @@ public class ProcessConfigConfiguration {
     @Bean
     public ProcessConfigService processConfigService(ProcessConfigRepository processConfigRepository) {
         return new ProcessConfigService(processConfigRepository);
+    }
+
+    @Bean
+    public ProcessConfigServiceWorkshop processConfigServiceWorkshop(ProcessConfigRepository processConfigRepository) {
+        return new ProcessConfigServiceWorkshop(processConfigRepository);
     }
 }
