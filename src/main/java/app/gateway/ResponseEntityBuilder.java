5685a1cfed88912887684984834b4b9db57066bc
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-18 22:23:47 +0100

    ResponseEntityBuilder removing unused methods

--- src/main/java/app/gateway/ResponseEntityBuilder.java
+++ src/main/java/app/gateway/ResponseEntityBuilder.java
@@ -7,15 +7,12 @@ import java.util.Optional;
 import java.util.function.Function;
 
 public class ResponseEntityBuilder {
+
     public static <T, Output> ResponseEntity<Output> okOrNotFound(Optional<T> domainObject, Function<T, Output> mapper) {
         return domainObject
-                .map(mapper::apply)
+                .map(mapper)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
-    }
-
-    public static <T, Output> ResponseEntity<Output> ok(T domainObject, Function<T, Output> mapper) {
-        return ResponseEntity.ok(mapper.apply(domainObject));
     }
 
     public static <T, Output> ResponseEntity<Output> created(T domainObject, Function<T, Output> mapper, Function<T, URI> uriMapper) {
