4407ce2910924685bafc44ed035926ba3a840e3f
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-22 20:01:29 +0100

    ResponseEntityBuilder.java utility class

--- src/main/java/app/gateway/ResponseEntityBuilder.java
+++ src/main/java/app/gateway/ResponseEntityBuilder.java
@@ -1,21 +1,23 @@
 package app.gateway;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class ResponseEntityBuilder {
+@UtilityClass
+class ResponseEntityBuilder {
 
-    public static <T, Output> ResponseEntity<Output> okOrNotFound(Optional<T> domainObject, Function<T, Output> mapper) {
+    <T, Output> ResponseEntity<Output> okOrNotFound(Optional<T> domainObject, Function<T, Output> mapper) {
         return domainObject
                 .map(mapper)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    public static <T, Output> ResponseEntity<Output> created(T domainObject, Function<T, Output> mapper, Function<T, URI> uriMapper) {
+    <T, Output> ResponseEntity<Output> created(T domainObject, Function<T, Output> mapper, Function<T, URI> uriMapper) {
         var uri = uriMapper.apply(domainObject);
         var body = mapper.apply(domainObject);
         return ResponseEntity.created(uri)
